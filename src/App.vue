015bf250da76653ced1e7351264324ee9b1b7e9a
danangyudhatama <danang.yudhatama@gmail.com>
2018-03-13 13:30:31 +0100

    first commit forked from tiamat edit the firebase config

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="sass">
+
+html,
+body,
+#app,
+.admin
+  background-color: #F1F1F1
+  height: 100%
+  width: 100%
+  margin: 0px
+  padding: 0px
+
+.bulma
+  @import "../node_modules/bulma/bulma"
+  *
+    box-sizing: border-box
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,28 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import VueFire from 'vuefire'
+import VueQuillEditor from 'vue-quill-editor'
+
+// import external stylesheets
+import '../node_modules/font-awesome/css/font-awesome.css'
+import '../node_modules/bulma/css/bulma.css'
+
+// import vue-quill-editor styles
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+Vue.use(VueFire) // activate vuefire plugin
+Vue.use(VueQuillEditor) // activate vue-quill-editor
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
